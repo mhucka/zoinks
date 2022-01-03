18adc432569a9c703cbc452fb92f9c4585f0a9a0
mhucka <mhucka@caltech.edu>
2022-01-03 07:14:33 -0800

    Add the usual setup.* files

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    setup.py
+# @brief   Installation setup file
+# @created 2021-10-16
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/mhucka/zoinks
+#
+# Note: configuration metadata is maintained in setup.cfg.  This file exists
+# primarily to hook in setup.cfg and requirements.txt.
+# =============================================================================
+
+import os
+from   os.path import exists, join, abspath, dirname
+from   setuptools import setup
+
+here = abspath(dirname(__file__))
+
+requirements = []
+if exists(join(here, 'requirements.txt')):
+    with open(join(here, 'requirements.txt')) as f:
+        requirements = f.read().rstrip().splitlines()
+
+setup(
+    setup_requires = ['wheel'],
+    install_requires = requirements,
+)
