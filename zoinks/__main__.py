904cd6cea3502dd4a5d37703ab1d341051668615
mhucka <mhucka@caltech.edu>
2022-01-03 11:36:28 -0800

    Rewrote many parts and rounded out features

--- zoinks/__main__.py
+++ zoinks/__main__.py
@@ -9,7 +9,7 @@ Michael Hucka <mhucka@caltech.edu> -- Caltech Library
 Copyright
 ---------
 
-Copyright (c) 2021 by Michael Hucka and the California Institute of Technology.
+Copyright (c) 2022 by Michael Hucka and the California Institute of Technology.
 This code is open-source software released under a 3-clause BSD license.
 Please see the file "LICENSE" for more information.
 '''
@@ -21,14 +21,22 @@ if sys.version_info <= (3, 8):
           str(sys.version_info.major) + '.' + str(sys.version_info.minor) + '.')
     exit(1)
 
-# Note: this code uses lazy loading.  Additional imports are made below.
-from   os.path import exists
+# Note: this code uses lazy loading.  Additional imports are made later.
+from   commonpy.data_structures import CaseFoldDict
+from   os.path import exists, isfile
 import plac
 from   sidetrack import set_debug, log
-from   uritools import urisplit, uriunsplit
 
 from   .exceptions import ZoinksError, ConnectionFailure
 from   .exit_codes import ExitCode
+
+
+# Constants.
+# .............................................................................
+
+_BBT_HOST = 'localhost'
+_BBT_PORT = 23119
+_BBT_JSON_RPC = f'http://{_BBT_HOST}:{int(_BBT_PORT)}/better-bibtex/json-rpc'
 
 
 # Main program.
@@ -62,24 +70,20 @@ def main(list_fields = False, source = 'S', no_gui = False,
     if version:
         from zoinks import print_version
         print_version()
-        exit(int(ExitCode.success))
+        sys.exit(int(ExitCode.success))
 
-    identifiers = []
+    fields = ['all'] if fields == () else list(field.lower() for field in fields)
+
     if source == 'S' or source.lower() in ['stdin', '-']:
-        if not sys.stdin.isatty():
-            identifiers = sys.stdin.read().split()
+        if sys.stdin.isatty():
+            stop('Input expected via pipe or redirection.', ExitCode.bad_arg)
         else:
-            stop('No input given.')
-    elif source.lower() == 'clipboard':
-        # The pasteboard code come from a 2011-11-29 posting by "MagerValp"
-        # to Stack Overflow at https://stackoverflow.com/a/8317794/743730
-        from AppKit import NSPasteboard, NSStringPboardType
-        pb = NSPasteboard.generalPasteboard()
-        pbstring = pb.stringForType_(NSStringPboardType)
-        identifiers = pbstring.split()
+            identifiers = sys.stdin.read().split()
+    elif source.lower() in ['clipboard', 'pasteboard']:
+        identifiers = clipboard().split()
         if not identifiers:
-            stop('The clipboard appears to be empty.')
-    elif exists(source):
+            stop('The clipboard appears to be empty.', ExitCode.bad_arg)
+    elif exists(source) and isfile(source):
         from commonpy.file_utils import readable
         if readable(source):
             with open(source, 'r') as file:
@@ -87,60 +91,72 @@ def main(list_fields = False, source = 'S', no_gui = False,
         else:
             stop('Unable to read file ' + source, ExitCode.file_error)
     else:
-        stop('File does not exist: ' + source, ExitCode.bad_arg)
-
+        stop('Path does not exist or is not a file: ' + source, ExitCode.bad_arg)
     identifiers = [id.strip() for id in identifiers]
-    fields = list(fields)
+
+    if not bbt_running():
+        stop('Zotero or Better BibTeX service does not appear to be running.',
+             ExitCode.zotero_error)
 
     # Do the real work --------------------------------------------------------
 
     try:
-        # BBT's JSON-RPC interface works off citation keys, so first get them.
-        # We get back a dict that maps item keys to citation keys.
-        log('asking BBT for citation keys for ' + str(identifiers))
-        mapping = bbt_rpc('item.citationkey', [identifiers])
+        # BBT's JSON-RPC interface needs citation keys. Get them if we need to.
+        if any(item_key(id).startswith('@') for id in identifiers):
+            log('user provided citation keys')
+            identifiers = [id.lstrip('@') for id in identifiers]
+            mapping = CaseFoldDict(zip(identifiers, identifiers))
+        else:
+            log('asking BBT for records for ' + str(identifiers))
+            mapping = bbt_rpc('item.citationkey', [identifiers])
+
+        single_record = (len(mapping) == 1)
+        single_field  = (len(fields) == 1)
 
         # If we're only getting citation keys, print them & exit.
-        if fields == ['citationkey']:
-            if len(mapping) == 1:
+        if single_field and fields[0] in ['citationkey', 'citekey']:
+            if single_record:
                 identifier, citationkey = mapping.popitem()
-                output(citationkey)
+                print(citationkey)
             else:
-                output('\n'.join(f'{k}:{v}' for k, v in mapping.items()))
-            exit(int(ExitCode.success))
+                print('\n'.join(f'{k}: {v}' for k, v in mapping.items()))
+            sys.exit(int(ExitCode.success))
 
         # Else, call BBT again with the citation keys to get the data of each
         # item, then replace the citation keys in our mapping with the records.
         # (The record has the citation key in it -- we're not losing anything.)
         log('getting record for each citation key from BBT')
-        for item_key, citation_key in mapping.items():
+        for key, citation_key in mapping.items():
             records = bbt_rpc('item.export', [[citation_key], 'jzon'])
             if records and len(records) == 1:
-                mapping[item_key] = records[0]
+                mapping[key] = records[0]
             elif records and len(records) > 1:
-                stop(f'Unexpectedly got {len(records)} records for {item_key}')
+                stop(f'Unexpectedly got {len(records)} records for {key}')
             else:
-                log(f'did not get a record for {item_key}')
-                mapping[item_key] = {'Unknown': 'Unknown'}
+                alert(f'did not get a record for {key}')
+                mapping[key] = {'Unknown': 'Unknown'}
 
         log('printing results')
         if list_fields:                 # Only list the field names, w/o values.
-            if len(mapping) == 1:
-                item_key, record = mapping.popitem()
+            if single_record:
+                key, record = mapping.popitem()
                 output(', '.join(record.keys()))
             else:
-                for item_key, record in mapping.items():
-                    output(item_key + ': ' + ', '.join(record.keys()), indent = '  ')
-                    output('\n')
-        elif fields == []:              # Print all field values.
-            for item_key, record in mapping.items():
+                for key, record in mapping.items():
+                    output(key + ': ' + ', '.join(record.keys()) + '\n', 2)
+        elif 'all' in fields:           # Print all field values.
+            for key, record in mapping.items():
                 pretty_print(record)
                 output('\n')
         else:                           # Print only selected field values.
-            for item_key, record in mapping.items():
-                for name in fields:
-                    output(name + ': ' + record.get(name, ''))
-
+            for key, record in mapping.items():
+                for name in [field for field in fields if field in record]:
+                    prefix = ''
+                    if not single_record:
+                        prefix = key + ': '
+                    if not single_field:
+                        prefix = (prefix + name + ': ')
+                    output(prefix + str(record.get(name, '')))
     except KeyboardInterrupt:
         log(f'user interrupted program -- exiting')
         sys.exit(int(ExitCode.user_interrupt))
@@ -162,65 +178,87 @@ def main(list_fields = False, source = 'S', no_gui = False,
 # Miscellaneous helpers.
 # .............................................................................
 
+def clipboard():
+    '''Return the clipboard/pasteboard contents as a string.'''
+    # The pasteboard code come from a 2011-11-29 posting by "MagerValp"
+    # to Stack Overflow at https://stackoverflow.com/a/8317794/743730
+    from AppKit import NSPasteboard, NSStringPboardType
+    pb = NSPasteboard.generalPasteboard()
+    return pb.stringForType_(NSStringPboardType)
+
+
+def item_key(x):
+    if x.startswith('zotero://'):
+        return x.rsplit('/', 1)[-1]
+    return x
+
+
+def bbt_running():
+    '''Return True if it looks like BBT is listening on the socket.'''
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex((_BBT_HOST, _BBT_PORT)) == 0
+
+
 def bbt_rpc(method, params):
     '''Call BBT's JSON-RPC interface and return the results.
 
-    This returns a tuple: (json dict, error).
-    The value of error will be None if no error occurred, else a string.
+    Returns a CaseFoldDict containing a dictionary version of the JSON
+    data returned by BBT for the query. If the query is for records (items) in
+    Zotero, returns a list of dictionaries.
+
+    Throws exceptions if something goes wrong.
     '''
-    from commonpy.network_utils import net
-    from commonpy.exceptions import Interrupted
+    from   commonpy.network_utils import net
+    from   commonpy.exceptions import Interrupted
     import json
 
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    }
-    data = json.dumps({
-        'jsonrpc': '2.0',
-        'method': method,
-        'params': params,
-    })
-    url = 'http://localhost:23119/better-bibtex/json-rpc'
-    (response, error) = net('post', url, headers = headers, data = data)
-    if error is None:
-        result_dict = json.loads(response.text)
-        if 'error' in result_dict.keys():
-            raise ZoinksError('BBT error: ' + result_dict['error']['message'])
-        if 'result' in result_dict.keys():
-            payload = result_dict['result']
-            if isinstance(payload, list) and isinstance(payload[0], int):
-                payload = json.loads(payload[2])
-            if 'items' in payload:
-                return payload['items']
-            return payload
-    else:
-        raise ConnectionFailure('Failed to connect to BBT API: ' + str(error))
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    data = json.dumps({'jsonrpc': '2.0', 'method': method, 'params': params})
+    (response, error) = net('post', _BBT_JSON_RPC, headers = headers, data = data)
+    if error:
+        raise ConnectionFailure('Better BibTeX API error: ' + str(error))
+    result_dict = json.loads(response.text)
+    if 'error' in result_dict.keys():
+        raise ZoinksError('BBT error: ' + result_dict['error']['message'])
+    if 'result' in result_dict.keys():
+        payload = result_dict['result']
+        if isinstance(payload, list) and isinstance(payload[0], int):
+            payload = json.loads(payload[2])
+        if 'items' in payload:
+            return [CaseFoldDict(item) for item in payload['items']]
+        return CaseFoldDict(payload)
+    return CaseFoldDict(result_dict)
 
 
-def pretty_print(record, indent=0):
+def zotero_record(identifier):
+    pass
+
+
+def pretty_print(record, indent = 0):
     for field, value in record.items():
         if isinstance(value, list):
             print(field + ':')
             for el in value:
-                pretty_print(el, indent + 4)
+                pretty_print(el, indent + 2)
         elif isinstance(value, dict):
-            pretty_print(value, indent + 4)
+            pretty_print(value, indent + 2)
         else:
-            print(' ' * indent +  field + ': ' + str(value))
+            print(' '*indent +  field + ': ' + str(value))
 
 
-def output(text, indent = ''):
+def output(text, indent = 0):
     import shutil
     from textwrap import wrap
     width = (shutil.get_terminal_size().columns - 2) or 78
-    print('\n'.join(wrap(text, width = width, subsequent_indent = indent)))
+    print('\n'.join(wrap(text, width = width, subsequent_indent = ' '*indent)))
 
 
 def inform(msg, no_gui):
+    '''Print an error message or show an error dialog.'''
     log('inform: ' + msg)
     if no_gui:
-        print('‼️  ' + msg)
+        print(msg)
     else:
         from osax import OSAX
         sa = OSAX("StandardAdditions", name = "System Events")
