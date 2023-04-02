7d40ae24998db23cd0fe5ee808bec965f111040b
Michael Hucka <mhucka@caltech.edu>
2023-04-02 13:15:28 -0700

    fix: issues with getting results from BBT

--- zoinks/__main__.py
+++ zoinks/__main__.py
@@ -53,11 +53,113 @@ _BBT_JSON_RPC = f'http://{_BBT_HOST}:{int(_BBT_PORT)}/better-bibtex/json-rpc'
 
 def main(list_fields = False, source = 'S', no_gui = False,
          version = False, debug = 'OUT', *fields):
-    '''Zoinks prints field values from Zotero records.'''
+    '''
+Zoinks accept one or more Zotero record identifiers together with zero or more
+field names, and prints the values of the field names on the standard output.
+If no field names are supplied, it prints all the field values it can find for
+each record.
+
+Zoinks requires Zotero to be running and have the Better BibTeX (BBT) extension
+installed, or it will exit with an error message.
+
+Identifying the desired Zotero records
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Zoinks understands the following forms of Zotero record identifiers:
+
+  * Zotero item keys (8 character alphanumeric sequences such as "WCKIQ23Y")
+  * Zotero select links (of the form "zotero://select/library/items/WCKIQ23Y")
+  * Citekeys (as managed by Better BibTeX) with leading '@' characters
+
+Identifiers can be supplied in any of the following ways:
+
+  * via standard input (e.g., from a shell pipe)
+  * from the clipboard (by using the value "clipboard" to the --source option)
+  * from a file (by using a file path as the value to the --source option)
+
+Here are some examples:
+
+  echo WCKIQ23Y | zoinks
+  echo @Smith2018 | zoinks
+  echo zotero://select/library/items/WCKIQ23Y | zoinks
+  zoinks --source clipboard
+  zoinks --source /tmp/zotero-identifiers.txt
+
+Important: Zoinks assumes that if more than one identifier is provided, all of
+the identifiers will have the same form. That is, if a Zotero select link is
+provided, then all the identifiers must be in the form of Zotero select links;
+if citekeys are provided, all the inputs must be citekeys; and so on.
+
+Specifying the field value(s) to be printed
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The field(s) whose value(s) is (are) to be printed can be named as arguments to
+Zoinks on the command. For example, the following command will print the title
+of the work identified by the given Zotero select link:
+
+  echo zotero://select/library/items/WCKIQ23Y | zoinks title
+
+To find out all the fields that are available, using the --list-fields option:
+
+  echo zotero://select/library/items/WCKIQ23Y | zoinks --list-fields
+
+If a single field value is requested, the value will be printed alone. If more
+than one field value is requested, each value in the output will be prefixed
+by the name of the field.  For example, the command
+
+  echo WCKIQ23Y | zoinks title
+
+might produce
+
+  IFLA Position on Controlled Digital Lending
+
+and the command
+
+  echo WCKIQ23Y | zoinks title date
+
+might produce
+
+  title: IFLA Position on Controlled Digital Lending
+  date: 2021
+
+If you do not name one or more fields on the command line, Zoinks will print
+all the fields returned by Better BibTeX for a given record. (Technically,
+the output will be equivalent to the "jzon" [sic] format provided by BBT.)
+
+Additional command-line arguments
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Zoinks will use GUI dialogs for error messages, unless the --no-gui option is
+given, in which case it will print messages on standard output.
+
+If given the -V option, this program will print the version and other
+information, and exit without doing anything else.
+
+If given the -@ argument, this program will output a detailed trace of what it
+is doing. The debug trace will be sent to the given destination, which can
+be '-' to indicate console output, or a file path to send the output to a file.
+
+Return values
+~~~~~~~~~~~~~
+
+This program exits with a return code of 0 if no problems are encountered.
+It returns a nonzero value otherwise. The following table lists the possible
+return values:
+
+  0 = success -- program completed normally
+  1 = the user interrupted the program's execution
+  2 = encountered a bad or missing value for an option
+  3 = encountered a problem with a file or directory
+  4 = encountered a problem communicating with Zotero or Better BibTeX
+  5 = a miscellaneous exception or fatal error occurred
+
+Command-line arguments summary
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
 
     # Define shortcut functions for common user feedback actions.
     def alert(msg): inform(msg, no_gui)
-    def stop(msg, err = ExitCode.exception): inform(msg, no_gui), sys.exit(int(err))
+    def stop(msg, err = ExitCode.exception): inform(msg, no_gui); sys.exit(int(err))
 
     # Process arguments & handle early exits ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -93,6 +195,7 @@ def main(list_fields = False, source = 'S', no_gui = False,
     else:
         stop('Path does not exist or is not a file: ' + source, ExitCode.bad_arg)
     identifiers = [id.strip() for id in identifiers]
+    item_keys = [item_key(id) for id in identifiers]
 
     if not bbt_running():
         stop('Zotero or Better BibTeX service does not appear to be running.',
@@ -101,20 +204,22 @@ def main(list_fields = False, source = 'S', no_gui = False,
     # Do the real work --------------------------------------------------------
 
     try:
-        # BBT's JSON-RPC interface needs citation keys. Get them if we need to.
-        if any(item_key(id).startswith('@') for id in identifiers):
-            log('user provided citation keys')
-            identifiers = [id.lstrip('@') for id in identifiers]
-            mapping = CaseFoldDict(zip(identifiers, identifiers))
+        log(f'input from source ' + source + ': ' + ', '.join(identifiers))
+        # BBT's JSON-RPC interface needs citation keys. If that's not what the
+        # user supplied, start by getting the citation keys from BBT.
+        if any(key.startswith('@') for key in item_keys):
+            bare_keys = [key.lstrip('@') for key in item_keys]
+            mapping = CaseFoldDict(zip(identifiers, bare_keys))
         else:
-            log('asking BBT for records for ' + str(identifiers))
-            mapping = bbt_rpc('item.citationkey', [identifiers])
+            log('need to retrieve citation keys from BBT')
+            mapping = bbt_rpc('item.citationkey', [item_keys])
 
         single_record = (len(mapping) == 1)
         single_field  = (len(fields) == 1)
 
         # If we're only getting citation keys, print them & exit.
         if single_field and fields[0] in ['citationkey', 'citekey']:
+            log('user requested only citation keys')
             if single_record:
                 identifier, citationkey = mapping.popitem()
                 print(citationkey)
@@ -208,22 +313,24 @@ def bbt_rpc(method, params):
     Throws exceptions if something goes wrong.
     '''
     from   commonpy.network_utils import net
-    from   commonpy.exceptions import Interrupted
     import json
 
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
     data = json.dumps({'jsonrpc': '2.0', 'method': method, 'params': params})
+    log('asking BBT about ' + str(params))
     (response, error) = net('post', _BBT_JSON_RPC, headers = headers, data = data)
     if error:
         raise ConnectionFailure('Better BibTeX API error: ' + str(error))
     result_dict = json.loads(response.text)
-    if 'error' in result_dict.keys():
+    if 'error' in result_dict:
         raise ZoinksError('BBT error: ' + result_dict['error']['message'])
-    if 'result' in result_dict.keys():
+    if 'result' in result_dict:
         payload = result_dict['result']
         if isinstance(payload, list) and isinstance(payload[0], int):
+            log('payload is a list; pulling out 3rd element')
             payload = json.loads(payload[2])
         if 'items' in payload:
+            log('got items in payload')
             return [CaseFoldDict(item) for item in payload['items']]
         return CaseFoldDict(payload)
     return CaseFoldDict(result_dict)
@@ -271,7 +378,7 @@ def inform(msg, no_gui):
 def console_scripts_main():
     plac.call(main)
 
-# The following allows users to invoke this using "python3 -m urial" and also
+# The following allows users to invoke this using "python3 -m zoinks" and also
 # pass it an argument of "help" to get the help text.
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'help':
