b7b47b32f386f6814e05e40ff2105310e0dc1ff3
Michael Hucka <mhucka@caltech.edu>
2023-12-22 16:48:18 -0800

    fix: OSAX constructor signature is different in v1.2.2

--- zoinks/__main__.py
+++ zoinks/__main__.py
@@ -362,7 +362,7 @@ def inform(msg, no_gui):
         print(msg)
     else:
         from osax import OSAX
-        sa = OSAX("StandardAdditions", name = "System Events")
+        sa = OSAX(name = "System Events")
         sa.activate()
         # The text below uses Unicode characters to produce bold text.
         sa.display_dialog('𝗭𝗼𝗶𝗻𝗸𝘀 𝗲𝗿𝗿𝗼𝗿:\n\n' + msg, buttons = ["OK"],
